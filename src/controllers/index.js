074ad8fa243880dfe83149e82ebd96f721eae8cd
Liliana Leiva <94813118+lilileiva@users.noreply.github.com>
2025-09-26 13:02:35 -0300

    feat: implement sendEmailController for email sending; refactor route to use controller

---
+++ src/controllers/index.js
@@ -0,0 +1,33 @@
+import sendEmail from "../utils/mailer.js";
+
+const sendEmailController = async (req, res) => {
+    if (
+        !req.body.from || !req.body.to || !req.body.subject
+    ) {
+        return res.status(400).json({
+            error: 'Missing required fields: from, to, subject'
+        });
+    };
+
+    if (
+        !req.body.text && (!req.body.title || !req.body.content || !req.body.footer
+
+        )) {
+        return res.status(400).json({
+            error: 'Missing either text field or required fields for HTML email: title, content, footer'
+        });
+    }
+
+    await sendEmail({
+        from: req.body.from,
+        to: req.body.to,
+        subject: req.body.subject,
+        title: req.body.title,
+        content: req.body.content,
+        footer: req.body.footer,
+        text: req.body.text,
+    }, req.file);
+    res.status(201).json({ message: 'Email sent successfully' });
+};
+
+export { sendEmailController };

0f9323f6aced7afc4fbee71b617ccdff286e8dbc
lilileiva <kaffidelili@gmail.com>
2026-01-31 10:48:53 -0300

    feat: add rate limit and helmet middlewares + update docs

--- src/app.js
+++ src/app.js
@@ -4,15 +4,16 @@ import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import yaml from "js-yaml";
 import fs from "fs";
-import router from './routes/index.js';
 import path from "path";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 
+import router from './routes/index.js';
 import requestLogMiddleware from "./middlewares/requestLog.middleware.js";
 import errorHandlerMiddleware from "./middlewares/errorHandler.middleware.js";
 import corsMiddleware from "./middlewares/cors.middleware.js";
 import apiKeyMiddleware from "./middlewares/apiKey.middleware.js";
 import { getLogger } from "./utils/logger.js";
-
 
 dotenv.config();
 
@@ -29,7 +30,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
+app.use(helmet());
 if (process.env.ENVIROMENT !== "local") {
   app.use(corsMiddleware);
   app.use(apiKeyMiddleware);
@@ -39,7 +40,11 @@ app.use(requestLogMiddleware);
 app.get('/healthz', (req, res) => {
   res.status(200).json({ status: "OK" });
 });
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiDocument));
+
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+
 app.use('', router);
 app.use((req, res, next) => {
   res.status(404).json({ error: "Not Found" });
